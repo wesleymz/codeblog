380ba436659830483c822c0bd0192587e1d8b13a
Wesley Santos Silva (SYSMAP) <wesleysilva.sysmap@latam.com>
2023-08-26 14:18:00 -0300

    Salvando um novo Post

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -2,12 +2,17 @@ package com.spring.codeblog.controller;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,5 +35,22 @@ public class CodeblogController {
         Post post = service.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @GetMapping(value = "/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping(value = "/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+
+        post.setData(LocalDate.now());
+        service.save(post);
+
+        return "redirect:/posts";
     }
 }
